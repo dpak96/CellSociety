681936ef229a0362d5f836f35a89bf69802e45bb
Daniel Pak <daniel1996@gmail.com>
2015-09-15 19:43:59 -0400

    XMLReader
    Created an XML Reader class that takes in the Grid and file name. Need
    to figure out how to set the rules from what is being read however.

---
+++ src/cellsociety_team05/Coordinates.java
@@ -0,0 +1,17 @@
+package cellsociety_team05;
+
+public class Coordinates {
+	private int myX;
+	private int myY;
+	
+	public Coordinates(int x, int y){
+		myX = x;
+		myY  = y;
+	}
+	public int getX(){
+		return myX;
+	}
+	public int getY(){
+		return myY;
+	}
+}
