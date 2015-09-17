0b8c8df793843ff006990a282ec2bb6539896a51
Daniel Pak <daniel1996@gmail.com>
2015-09-16 21:29:16 -0400

    Edit XMLReader
    Added getters to XMLReader to get setup information. Added this
    information to setup.

--- src/cellsociety_team05/XMLReader.java
+++ src/cellsociety_team05/XMLReader.java
@@ -20,14 +20,15 @@ public class XMLReader {
 	private static String SIMNAME = "simulation";
 	private static String TAGNAME = "cell";
 	private static String PARAM = "parameters";
-			private  Grid myGrid; 
+	private static String GRID = "grid";
 	private HashMap<String, Double> parameters; 
 	private int rule;
+	private int width;
+	private int height;
 
-	public XMLReader(String file, Grid grid){
+	public XMLReader(String file){
 		fileName = file;
 		cells = new ArrayList<Cell>();
-		myGrid = grid;
 	}
 
 	public void readFile(){
@@ -53,13 +54,18 @@ public class XMLReader {
 
 			//Getting parameters
 			nList = doc.getElementsByTagName(PARAM);
-			for (int temp = 0; temp < nList.getLength(); temp++) {
+			nNode = nList.item(0);
+			Element eElement = (Element) nNode;
 
-				nNode = nList.item(temp);
-				Element eElement = (Element) nNode;
-				parameters.put(eElement.getAttribute("name"), Double.parseDouble(eElement.getAttribute("val")));
-				}
-				
+			parameters.put(eElement.getAttribute("name"), Double.parseDouble(eElement.getAttribute("val")));
+
+			//Getting grid dimensions
+			nList = doc.getElementsByTagName(GRID);
+			nNode = nList.item(0);
+			eElement = (Element) nNode;
+			width = Integer.parseInt(eElement.getAttribute("width"));
+			height = Integer.parseInt(eElement.getAttribute("height"));
+
 
 			//Getting cells
 			nList = doc.getElementsByTagName(TAGNAME);
@@ -72,11 +78,11 @@ public class XMLReader {
 
 				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 
-					Element eElement = (Element) nNode;
+					eElement = (Element) nNode;
 					int x = Integer.valueOf(eElement.getElementsByTagName("xcord").item(0).getTextContent());
 					int y = Integer.valueOf(eElement.getElementsByTagName("ycord").item(0).getTextContent());
 					int state = Integer.valueOf(eElement.getElementsByTagName("state").item(0).getTextContent());
-					cells.add(new Cell(myGrid, x,y,state));
+					cells.add(new Cell(x,y,state));
 
 				}
 			}
@@ -84,7 +90,19 @@ public class XMLReader {
 			e.printStackTrace();
 		}
 	}
-	public HashMap getParams(){
+	public HashMap<String, Double> getParams(){
 		return parameters;
+	}
+	public ArrayList<Cell> getCells(){
+		return cells;
+	}
+	public String getSimulation(){
+		return simulation;
+	}
+	public int getGridWidth(){
+		return width;
+	}
+	public int getGridHeight(){
+		return height;
 	}
 }
